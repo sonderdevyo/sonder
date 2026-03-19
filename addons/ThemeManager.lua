886283a1a6e205a45d0b3b99b7d250f1b9f4a260
sonder yo <leonblack2534@gmail.com>
2026-03-19 19:00:16 +0100

    Fix JSON formatting in Default theme definition

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -5,13 +5,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Default'] = { 1, httpService:JSONDecode('{
-	"FontColor":"ffffff",
-	"MainColor":"1a1a1a",
-	"AccentColor":"ff4d8d",
-	"BackgroundColor":"121212",
-	"OutlineColor":"2a2a2a"
-}') },
+		['Default'] = { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1a1a1a","AccentColor":"ff4d8d","BackgroundColor":"121212","OutlineColor":"2a2a2a"}') },
 		['BBot'] 			= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Fatality']		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
 		['Jester'] 			= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
